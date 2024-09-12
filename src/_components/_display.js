f442507da5c62bfe92ee00ae9fe983c23483456d
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-09-13 00:20:12 +1000

    Update the methods of the Display class to static

--- src/_components/_display.js
+++ src/_components/_display.js
@@ -1,12 +1,24 @@
+import Navigation from "./navigation";
 import Search from "./search";
+import WeatherToday from "./weatherToday";
+import WeatherForecast from "./weatherForecast";
 
 export default class Display {
-  constructor() {}
-
-  refresh() {}
-  render() {
+  static refresh(data) {
+    const main = document.querySelector("main");
+    main.innerHTML = `
+      ${new WeatherToday(data).render()}
+      ${new WeatherForecast(data).render()}
+    `;
+  }
+  static render(data) {
     return `
-      ${new Search().render()}
+      ${new Navigation(data).render()}
+      ${new Search(data).render()}
+      <main>
+        ${new WeatherToday(data).render()}
+        ${new WeatherForecast(data).render()}
+      </main>
     `;
   }
 }
